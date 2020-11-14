a4b7e1a67ec30a0b3f37140e2c464be6e204b8be
Eperjesi Attila Dávid <eper.atis@gmail.com>
2020-11-14 11:32:14 +0100

    Sidebar and Routing
    Sidebar added.
    Routing added.
    
    Ref: #22 #23

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import './App.scss';
+import {Container, Row, Col } from "react-bootstrap";
 import ComplexNumberRecordingForm from "./component/ComplexNumberRecordingForm";
 import ComplexNumberList from "./component/ComplexNumberList";
 import CustomerList from "./component/CustomerList";
 import CustomerRecordingForm from "./component/CustomerRecordingForm";
+import Sidebar from "./component/Sidebar";
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect
+} from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className={["App","container"]}>
-        <div className={"row"}>
-            <div className={"col-md-3"}></div>
-            <div className={"col-md-6"}>
-                <CustomerRecordingForm/>
-                <CustomerList/>
-            </div>
-            <div className={"col-md-3"}></div>
-        </div>
-    </div>
+      <Router>
+          <Container fluid>
+              <Row>
+                  <Col xs={2} id="sidebar-wrapper">
+                      <Sidebar />
+                  </Col>
+                  <Col  xs={10} id="page-content-wrapper">
+                      <div className={["App","container"]}>
+                          <div className={"row"}>
+                              <div className={"col-md-3"}></div>
+                              <div className={"col-md-6"}>
+                                  <Switch>
+                                      {/*<Route exact path="/slot/status" component={Slots}/>*/}
+                                      <Route exact path="/customer" component={CustomerList}/>
+                                      <Route exact path="/customer/record" component={CustomerRecordingForm}/>
+                                      {/*<Route exact path="/404" component={NotFound}/>*/}
+                                      <Redirect to="/404"/>
+                                      <CustomerRecordingForm/>
+                                      <CustomerList/>
+                                  </Switch>
+                              </div>
+                              <div className={"col-md-3"}></div>
+                          </div>
+                      </div>
+                  </Col>
+              </Row>
+
+          </Container>
+      </Router>
   );
 }
 
