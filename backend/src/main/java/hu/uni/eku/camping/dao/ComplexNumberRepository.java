723fe6cce5673dea7994308d486a3f7855b12bc6
tamas-balog <balog.adalbert.tamas@gmail.com>
2020-11-16 11:43:46 +0100

    Delete unnecessary classes
    Ref: #40

--- backend/src/main/java/hu/uni/eku/camping/dao/ComplexNumberRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.uni.eku.camping.dao;
-
-import hu.uni.eku.camping.dao.entity.ComplexNumber;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ComplexNumberRepository extends CrudRepository<ComplexNumber, Integer> {
-}
